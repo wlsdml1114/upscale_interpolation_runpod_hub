d9b18ef96b5933280ee395253acd49acd1fc94fd
wlsdml1114 <wlsdml1114@gmail.com>
2026-01-29 00:29:16 +0900

    remove sage attention

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 
 # Start ComfyUI in the background
 echo "Starting ComfyUI in the background..."
-python /ComfyUI/main.py --listen --use-sage-attention &
+python /ComfyUI/main.py --listen &
 
 # Wait for ComfyUI to be ready
 echo "Waiting for ComfyUI to be ready..."
